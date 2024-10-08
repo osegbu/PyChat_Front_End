cc7fe219f34668f8124188fc26dfb6286c895e74
osegbu <valentineosegbu.com>
2024-10-08 17:42:58 +0100

    Updated

--- src/components/currentUser/CurrentUser.jsx
+++ src/components/currentUser/CurrentUser.jsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
 import { useCallback, memo } from "react";
 import styles from "./user.module.css";
-import logoutIcon from "@/icons/logout.png";
+import logout from "@/icons/logout.png";
 import { signOut } from "next-auth/react";
 import { handleLogout } from "@/lib/action";
 import { deleteDatabase } from "../websocket/dbUtils";
 
-const CurrentUser = ({ logout }) => {
+const CurrentUser = () => {
   const logOutUser = useCallback(async () => {
     const result = await handleLogout();
     if (result.success) {
-      logout();
       await deleteDatabase();
       await signOut();
     }
@@ -20,7 +19,7 @@ const CurrentUser = ({ logout }) => {
     <div className={styles.container}>
       <div className={styles.appName}>PyChat</div>
       <button onClick={logOutUser} title="Logout">
-        <Image src={logoutIcon} alt="Logout Button" height={24} width={24} />
+        <Image src={logout} alt="Logout Button" height={24} width={24} />
       </button>
     </div>
   );
