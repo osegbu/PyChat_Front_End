01003d19640e6200bff077fe2b7c8f46b9dba3b0
osegbu <valentineosegbu.com>
2024-10-09 15:38:53 +0100

    Updated

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -1,31 +1,23 @@
 "use client";
-import { useSession, getSession } from "next-auth/react";
+import { getSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import HomeComponent from "./homeComponent/HomeComponent";
 
 export const LandingPage = () => {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const checkSession = async () => {
-      setIsLoading(true);
       const currentSession = await getSession();
 
       if (!currentSession) {
         router.replace("/login");
-      } else {
-        setIsLoading(false);
       }
     };
 
     checkSession();
   }, [router]);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
 
   return <HomeComponent />;
 };
